9b8ead6f754e7ab57a0ab5d4139ebbabadbe3fe3
kpatel427 <khushbu_p@hotmail.com>
2019-07-24 08:53:51 -0400

    scripts divided into server and ui

---
+++ app/server.R
@@ -0,0 +1,129 @@
+# SERVER SCRIPT
+
+#1. FETCH DATA from Envir------------------------
+data(iris)
+temp <- ls()[sapply(ls(), function(x) class(get(x))) == 'data.frame']
+
+
+# 2. SOURCE all plot scripts-----------------------
+sapply(list.files("plots",full.names = T),source)
+
+
+# 3. SERVER Starts Here------------------------
+  
+  server = function(input, output, session) {
+    
+    #___3.1 SERVER: Refresh Function
+    observeEvent(input$refresh1, {
+      shinyjs::js$refresh()
+    }) 
+    
+    
+    #___3.2 SERVER : Reading Data from file--------------
+    
+    data <-  reactive({
+      if(!is.null(input$file1)){
+        inFile <- input$file1
+        read.csv(inFile$datapath) 
+        
+      } else {
+        if(input$tableName!="None"){
+          get(input$tableName, envir = .GlobalEnv)
+        }
+        
+      }
+    })
+    
+    
+    
+    #___3.3 SERVER : Update selectInputs--------------
+    
+    observeEvent(data(), {
+      updateSelectInput(session, inputId = "selectX", choices=colnames(data()))
+      updateSelectInput(session, inputId = "selectY", choices=colnames(data()))
+      
+      # update plot parameter dropdowns
+      colorby.choices <- append(colnames(data()),'None')
+      updateSelectInput(session, inputId = "colorby", choices=colorby.choices, selected = 'None')
+    })
+    
+    #___3.4 SERVER : Displaying Data (sanity check!) ---------
+    output$tabout <- renderTable({
+      
+      if(is.null(data())){
+        return()
+      }
+      data()
+    })
+    
+    #___3.5 SERVER : Input Type CHECK ---------
+    
+    #___3.5.1 INPUT TYPE CHECK: All 3 Conditions Covered ---------
+    
+    observeEvent(c(input$selectX,input$selectY), {
+      dt <- data()
+      if(!is.numeric(dt[[input$selectX]]) & !is.numeric(dt[[input$selectY]])){
+        updateRadioGroupButtons(session = session,inputId = "radioPlot",
+                                choices = c(""),
+                                checkIcon = list(yes = icon("ok", 
+                                                            lib = "glyphicon")),
+                                selected = F,
+                                status = "warning")
+      } else if(!is.numeric(dt[[input$selectX]]) | !is.numeric(dt[[input$selectY]])) {
+        
+        updateRadioGroupButtons(session = session,inputId = "radioPlot",
+                                choices = c("Bar", "Line"),
+                                checkIcon = list(yes = icon("ok", 
+                                                            lib = "glyphicon")),
+                                selected = F,
+                                status = "warning")              
+      } else {
+        
+        updateRadioGroupButtons(session = session,inputId = "radioPlot",
+                                choices = c("Bar", "Scatter","Line"),
+                                checkIcon = list(yes = icon("ok", 
+                                                            lib = "glyphicon")),
+                                selected = F,
+                                status = "warning") 
+      }
+    })
+    
+    #4 PLOTS CODE: -------------
+    output$basic_barplot <- renderPlot({
+      dt <- data()
+      if(is.null(dt)){return()}
+      if(is.null(input$radioPlot)){return()}
+      
+      switch(input$radioPlot,
+             "Bar" =    bar_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                 fontSize = input$axisFont, legendPos = input$legendPosition)$plot,
+             "Scatter" = scatter_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                      fontSize = input$axisFont, legendPos = input$legendPosition,
+                                      dotSize = input$dotSize)$plot,
+             "Line" =    line_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                   fontSize = input$axisFont, legendPos = input$legendPosition)$plot
+      )
+      
+    })
+    
+    
+    #5 RETURN CODE BLOCK: -------------
+    output$return_code <- renderText({
+      dt <- data()
+      if(is.null(dt)){return()}
+      if(is.null(input$radioPlot)){return()}
+      
+      switch(input$radioPlot,
+             "Bar" =    bar_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                 fontSize = input$axisFont, legendPos = input$legendPosition)$code,
+             "Scatter" = scatter_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                      fontSize = input$axisFont, legendPos = input$legendPosition,
+                                      dotSize = input$dotSize)$code,
+             "Line" =    line_plot(data = dt,x=input$selectX,y=input$selectY, colorby = input$colorby, 
+                                   fontSize = input$axisFont, legendPos = input$legendPosition)$code
+      )
+      
+    })
+    
+} # server ends here
+  
---
+++ app/ui.R
@@ -0,0 +1,135 @@
+# UI script
+library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
+library(shinyFiles)
+library(shinycssloaders)
+library(htmltools)
+library(ggplot2)
+library(R.utils)
+
+
+ui <- dashboardPage(
+  # 1. HEADER here----
+  dashboardHeader(title = "ggEasyPlot",
+                  titleWidth = 300,
+                  tags$li(id="refresh1",a(onclick = "window.location.href=window.location.href",
+                                          href = NULL,
+                                          tags$p(tags$i(class="fa fa-refresh fa-spin",style="font-size:18px"),HTML("&nbsp;")," Reload Data"),
+                                          #HTML('<i class="fa fa-refresh fa-spin" style="font-size:18px"></i>'),
+                                          title = "Refresh",
+                                          style = "cursor: pointer; font-size: 16px;, face:bold;"),
+                          class = "dropdown",
+                          tags$script(HTML("
+                                           var openTab = function(tabName){
+                                           $('a', $('.sidebar')).each(function() {
+                                           if(this.getAttribute('data-value') == tabName) {
+                                           this.click()
+                                           };
+                                           });
+                                           }")))
+                  ),
+  # 2. SIDEBAR here----
+  dashboardSidebar(sidebarMenu(
+                        # Setting id makes input$tabs give the tabName of currently-selected tab
+                        id = "tabs",
+                        menuItem("Home", tabName = "summary", icon = icon("dashboard"))
+                        
+                      ),collapsed = T
+  ),
+  
+  # 3. BODY here----
+  dashboardBody(
+    includeCSS("../template.css"),
+    shinyjs::useShinyjs(),
+    
+    tabItem("summary",
+            
+            #__3.1 Info boxes ---------------
+            fluidRow(
+              div(class="input_box", style="width:50%",
+                  box(status = "primary",
+                      h4("Select input params:"),
+                      fileInput("file1", "Upload data file (csv/txt/tsv):",
+                                accept = c("text/csv",
+                                           "text/comma-separated-values,text/plain",
+                                           ".csv")
+                      ),
+                      
+                      selectInput(
+                        inputId = "tableName", label = "Select R Env Data ",
+                        selected = "None", multiple = FALSE,
+                        choices = c(temp,"None")),
+                      #__3.2 DropDowns for X and Y axis-------------
+                      selectInput(inputId = "selectX", label = "Select X-axis variable:", choices = ''),
+                      selectInput(inputId = "selectY", label = "Select Y-axis variable:", choices = ''),
+                      
+                      #__3.3 Add radios to choose type of plot------------
+                      radioGroupButtons(
+                        inputId = "radioPlot",
+                        label = "Select Plot Type",
+                        choices = c("Bar", "Scatter", "Line"),
+                        justified = TRUE,
+                        checkIcon = list(yes = icon("ok", 
+                                                    lib = "glyphicon")),
+                        selected = F,
+                        status = "warning"
+                      ),
+                      
+                      #__3.4 interactive or no?------------
+                      prettySwitch(
+                        inputId = "interact",
+                        label = "Interactive plot", 
+                        status = "primary",
+                        slim = TRUE
+                      ),
+                      
+                      tags$h4(tags$b("Code :") ,tags$br(),tags$br(),verbatimTextOutput('return_code'),color = '#3c8dbc')
+                  )
+                  
+              ),
+              
+              div(class="output_box", style="width:150%;",
+                  #tabBox(status="info","Plot Output", withSpinner(plotOutput('basic_barplot',height = '600px'),color = '#3c8dbc'),value = 'basic_barplot')
+                  tabBox( height = '663px',title = 
+                            dropdownButton(
+                              tags$h3("Set parameters"),
+                              #selectInput(inputId = 'xcol', label = 'X Variable', choices = names(iris)),
+                              selectInput(inputId = 'colorby', label = 'Color by', choices = c('None')),
+                              #selectInput(inputId = 'selectTheme', label = 'Choose Theme', choices = c(theme_bw(), theme_classic()), selected = theme_bw()),
+                              sliderInput(inputId = 'axisFont', label = 'Font Size', value = 10, min = 1, max = 50),
+                              radioGroupButtons(
+                                inputId = "legendPosition",
+                                label = "Legend Position",
+                                choices = c("top"='top', 
+                                            "bottom"='bottom', 
+                                            "left"='left', 
+                                            "right"='right'),
+                                selected = 'right',
+                                justified = TRUE,
+                                checkIcon = list(
+                                  yes = icon("ok", 
+                                             lib = "glyphicon"))
+                              ),
+                              conditionalPanel(condition = "input.radioPlot == 'Bar'",
+                                               selectInput(inputId = 'test1', label = 'Barplot specific Input', choices = c('None'))
+                              ),
+                              conditionalPanel(condition = "input.radioPlot == 'Scatter'",
+                                               sliderInput(inputId = 'dotSize', label = 'Dot Size', value = 2, min = 1, max = 20)
+                              ),
+                              #selectInput(inputId = 'ycol', label = 'Y Variable', choices = names(iris), selected = names(iris)[[2]]),
+                              #sliderInput(inputId = 'clusters', label = 'Cluster count', value = 3, min = 1, max = 9),
+                              circle = TRUE, status = "danger", icon = icon("gear"), width = "300px",
+                              tooltip = tooltipOptions(title = "Click to see inputs !")
+                            ),
+                          tabPanel(tagList(shiny::icon("table"),""), withSpinner(plotOutput('basic_barplot',height = '563px'),color = '#3c8dbc'),value = 'plot6')
+                          
+                  ) # end of tabBox 
+              )#end of div
+              
+            ) #end of fluid row
+            
+    ) #end of tabItems Summary
+    
+  ) # dashboard Body ends here
+) # UI ends here
